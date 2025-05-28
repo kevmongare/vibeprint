fd750c3ebfb588d13c76cb4f8853c75beef9149c
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-05-28 19:18:07 +0300

    updating the vibeprint logo span

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
+    //Menu bar
       <section  className='position-top sticky px-5 md:px-25 py-5 text-gray-800 postition-0 w-full shadow-sm left-0 top-0 mx-auto justify-between flex items-center bg-white/80 md:py-0 z-50'>
-      <h1 className='flex items-center md:text-2xl text-sm font-extrabold text-orange-600'><img src= "" alt="" className='h-20 hidden md:flex' />VitePrint</h1>
+      <h1 className='flex items-center md:text-2xl text-sm font-extrabold text-orange-600'><img src= "" alt="" className='h-20 hidden md:flex' />Vite<span className='text-blue-600'>Print</span></h1>
     
         <ul className='flex items-center'>
           <a href="" className='mr-5 text-1xl hover:text-orange-600 text-gray-800'> Home </a>
