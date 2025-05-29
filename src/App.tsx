3ba81a6942f61089b12e26f0363559527ad6899d
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-05-29 13:37:54 +0300

    update on the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -146,7 +146,7 @@ function App() {
                 </div>
             </div>
             <div className="mt-6">
-            <iframe className = "md:w-150 w-80 h=150 shadow-2xl rounded-2xl referrerpolicy="
+            <iframe className = "md:w-150 w-50 h=150 shadow-2xl rounded-2xl referrerpolicy="
             loading="lazy"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3915.9221470790144!2d36.94046947487992!3d-1.3974949985893008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f0d0078fdcce7%3A0xdde3324ee69cae62!2sMlolongo%20gossip!5e1!3m2!1sen!2ske!4v1747402783315!5m2!1sen!2ske">
             
